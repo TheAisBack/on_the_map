e2f2e9b29782447c01ebe4b473cc8ecc39b7d63d
Theaisback <alanhekle@hotmail.com>
2017-09-22 23:40:10 -0500

    adding files

---
+++ on_the_map/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  on_the_map
+//
+//  Created by Alan Joseph Hekle on 2017-09-22.
+//  Copyright © 2017 Alan Joseph Hekle. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
